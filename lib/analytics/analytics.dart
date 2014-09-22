796b132771a6a126ae7131d0f2bb26905970b927
Devon Carew <devoncarew@gmail.com>
2014-09-21 21:52:09 -0700

    simplify and cleanup the analytics library

--- lib/analytics/analytics.dart
+++ lib/analytics/analytics.dart
@@ -2,182 +2,69 @@
 // All rights reserved. Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// TODO: document how to support an opt-out workflow
+
+// TODO: document how to support an opt-in workflow
+
 /**
  * TODO:
  */
 library stagehand.analytics;
 
-import 'dart:async';
-import 'dart:math' as math;
-
-import 'package:uuid/uuid.dart';
-
-// TODO: Under construction!
-
-// TODO: store a generated anonymous client id
-
-// TODO: store whether the user has opted in / out
-
-// https://developers.google.com/analytics/devguides/collection/protocol/policy
-
-class Analytics {
-  static const String _GA_URL = 'https://www.google-analytics.com/collect';
-
-  /// Tracking ID / Property ID.
-  final String trackingId;
-  final PropertiesHandler propertiesHandler;
-  final PostHandler postHandler;
-  final String applicationVersion;
-  final String applicationId;
-
-  String applicationName;
-
-  final _ThrottlingBucket _bucket = new _ThrottlingBucket(20);
-
-  Analytics(this.trackingId, this.propertiesHandler, this.postHandler,
-      {this.applicationVersion, this.applicationId}) {
-    assert(trackingId != null);
-
-    _initClientId();
-  }
-
-  /**
-   * Anonymous Client ID. The value of this field should be a random UUID
-   * (version 4).
-   */
-  String get _clientId => propertiesHandler.getProperty('clientId');
-
-  Future sendScreenView(String viewName) {
-    Map args = {'cd': viewName};
-    return _sendPayload('screenview', args);
-  }
-
-  Future sendEvent(String category, String action, [String label]) {
-    Map args = {'ec': category, 'ea': action};
-    if (label != null) args['el'] = label;
-    return _sendPayload('event', args);
-  }
-
-  Future sendException(String description, [bool fatal]) {
-    Map args = {'exd': description};
-    if (fatal != null && fatal) args['exf'] = '1';
-    return _sendPayload('exception', args);
-  }
-
-  void _initClientId() {
-    if (_clientId == null) {
-      propertiesHandler.setProperty('clientId', new Uuid().v4());
-    }
-  }
-
-  // Valid values for [hitType] are: 'pageview', 'screenview', 'event',
-  // 'transaction', 'item', 'social', 'exception', and 'timing'.
-  Future _sendPayload(String hitType, Map args) {
-    assert(hitType != null);
-
-    if (_bucket.removeDrop()) {
-      args['v'] = '1'; // version
-      args['tid'] = trackingId;
-      args['cid'] = _clientId;
-      args['t'] = hitType;
-
-      if (applicationId != null) args['aid'] = applicationId;
-      if (applicationVersion != null) args['av'] = applicationVersion;
-      if (applicationName != null) args['an'] = applicationName;
-
-      return postHandler.sendPost(_GA_URL, args);
-    } else {
-      return new Future.value();
-    }
-  }
-}
-
 /**
  * TODO:
  */
-abstract class PropertiesHandler {
-  dynamic getProperty(String key);
-  void setProperty(String key, dynamic value);
-}
-
-/**
- * TODO:
- */
-abstract class PostHandler {
+abstract class Analytics {
   /**
-   * TODO:
+   * Whether analytics has been disabled by the user. Also, enable or disable
+   * analytics.
    */
-  Future sendPost(String url, Map<String, String> parameters);
+  bool disabled;
 
-  String postEncode(Map<String, String> map) {
-    // &foo=bar
-    return map.keys
-        .map((key) => "${key}=${Uri.encodeComponent(map[key])}")
-        .join('&');
-  }
+  /**
+   * Whether the [disabled] value has been explicitly set (either enabled or
+   * disabled).
+   */
+  bool get enablementExplicitlyChanged;
+
+  void sendScreenView(String viewName);
+  void sendEvent(String category, String action, [String label]);
+  void sendException(String description, [bool fatal]);
 }
 
-class MockPropertiesHandler implements PropertiesHandler {
-  Map<String, dynamic> _map = {};
+class AnalyticsMock extends Analytics {
+  final bool logCalls;
 
-  dynamic getProperty(String key) => _map[key];
+  bool disabled = false;
+  bool enablementExplicitlyChanged = false;
 
-  void setProperty(String key, dynamic value) {
-    _map[key] = value;
-  }
-}
+  AnalyticsMock([this.logCalls = false]);
 
-class MockPostHandler extends PostHandler {
-  final bool _doPrint;
-
-  MockPostHandler([this._doPrint = false]);
-
-  Future sendPost(String url, Map<String, String> parameters) {
-    if (_doPrint) {
-      String data = postEncode(parameters);
-      print('[${url}: ${data}]');
-    }
-
-    return new Future.value();
-  }
-}
-
-/**
- * A throttling algorithim. This models the throttling after a bucket with
- * water dripping into it at the rate of 1 drop per second. If the bucket has
- * water when an operation is requested, 1 drop of water is removed and the
- * operation is performed. If not the operation is skipped. This algorithim
- * lets operations be peformed in bursts without throttling, but holds the
- * overall average rate of operations to 1 per second.
- */
-class _ThrottlingBucket {
-  final int startingCount;
-  int drops;
-  int _lastReplenish;
-
-  _ThrottlingBucket(this.startingCount) {
-    drops = startingCount;
-    _lastReplenish = new DateTime.now().millisecondsSinceEpoch;
+  void sendScreenView(String viewName) {
+    if (logCalls) _log('screenView', viewName);
   }
 
-  bool removeDrop() {
-    _checkReplenish();
-
-    if (drops <= 0) {
-      return false;
-    } {
-      drops--;
-      return true;
+  void sendEvent(String category, String action, [String label]) {
+    if (logCalls) {
+      if (label == null) {
+        _log('event', '${action}');
+      } else {
+        _log('event', '${action},${label}');
+      }
     }
   }
 
-  void _checkReplenish() {
-    int now = new DateTime.now().millisecondsSinceEpoch;
-
-    if (_lastReplenish + 1000 >= now) {
-      int inc = (now - _lastReplenish) ~/ 1000;
-      drops = math.min(drops + inc, startingCount);
-      _lastReplenish += (1000 * inc);
+  void sendException(String description, [bool fatal]) {
+    if (logCalls) {
+      if (fatal == null) {
+        _log('event', '${description}');
+      } else {
+        _log('event', '${description},fatal=${fatal}');
+      }
     }
+  }
+
+  void _log(String hitType, String message) {
+    print('analytics: ${hitType} ${message}');
   }
 }
--- test/all.dart
+++ test/all.dart
@@ -2,14 +2,14 @@
 // All rights reserved. Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+import 'analytics_impl_test.dart' as analytics_impl_test;
 import 'cli_test.dart' as cli_test;
 import 'common_test.dart' as common_test;
 import 'generators_test.dart' as generators_test;
-import 'properties_io_test.dart' as properties_io_test;
 
 void main() {
+  analytics_impl_test.defineTests();
   cli_test.defineTests();
   common_test.defineTests();
   generators_test.defineTests();
-  properties_io_test.defineTests();
 }
