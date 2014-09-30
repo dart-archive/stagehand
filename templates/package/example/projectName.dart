e8e0b5dd112f6ba0e6918025ca3cd80655901c75
Devon Carew <devoncarew@gmail.com>
2014-09-29 23:03:03 -0700

    use a dir template for the package example

---
+++ templates/package/example/projectName.dart
@@ -0,0 +1,12 @@
+// Copyright (c) {{year}}, the {{projectName}} project authors. Please see the
+// AUTHORS file for details. All rights reserved. Use of this source code is
+// governed by a BSD-style license that can be found in the LICENSE file.
+
+library {{projectName}}.example;
+
+import 'package:{{projectName}}/{{projectName}}.dart';
+
+main() {
+  var awesome = new Awesome();
+  print(awesome.isAwesome);
+}
