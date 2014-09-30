4b2081d0073a06ed80b9775bbfbde416470e7d65
Seth Ladd <sethladd@gmail.com>
2014-09-29 21:50:09 -0700

    fix library name

--- lib/generators/polymer.dart
+++ lib/generators/polymer.dart
@@ -2,7 +2,7 @@
 // All rights reserved. Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-library stagehand.webapp;
+library stagehand.polymer;
 
 import '../stagehand.dart';
 import '../src/common.dart';
