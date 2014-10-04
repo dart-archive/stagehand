595188566dd68a8a1f72d680b600b70c551f0659
Devon Carew <devoncarew@gmail.com>
2014-10-04 14:17:24 -0700

    fix build

--- templates/consoleapp/bin/main.dart
+++ templates/consoleapp/bin/main.dart
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-import 'package:{{projectName}}/{{projectName}}.dart';
+import 'package:{{projectName}}/{{projectName}}.dart' as {{projectName}};
 
 main() {
   print('Hello world!');
