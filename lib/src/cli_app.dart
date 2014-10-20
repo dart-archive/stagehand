1175ce49f4d0188900aec93f0fb077b88166ad78
Seth Ladd <sethladd@gmail.com>
2014-10-20 10:11:20 -0700

    bump version to 0.1.0

--- lib/src/cli_app.dart
+++ lib/src/cli_app.dart
@@ -18,7 +18,7 @@ import 'package:stagehand/src/common.dart';
 const String APP_NAME = 'stagehand';
 
 // This version must be updated in tandem with the pubspec version.
-const String APP_VERSION = '0.0.5-dev';
+const String APP_VERSION = '0.1.0';
 
 // The Google Analytics tracking ID for stagehand.
 const String _GA_TRACKING_ID = 'UA-55033590-1';
