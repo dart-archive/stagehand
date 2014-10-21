56ddcc58f93ac72f1a6dfc6a10b58caedb740c21
Kevin Moore <github@j832.com>
2014-10-21 13:49:13 -0700

    shelfapp: add command line flag for port
    Closes google/stagehand#89

--- templates/shelfapp/bin/server.dart
+++ templates/shelfapp/bin/server.dart
@@ -1,15 +1,28 @@
 // Copyright (c) {{year}}, <your name>. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
+import 'dart:io';
+
+import 'package:args/args.dart';
 import 'package:shelf/shelf.dart' as shelf;
 import 'package:shelf/shelf_io.dart' as io;
 
-void main() {
+void main(List<String> args) {
+  var parser = new ArgParser()
+      ..addOption('port', abbr: 'p', defaultsTo: '8080');
+
+  var result = parser.parse(args);
+
+  var port = int.parse(result['port'], onError: (val) {
+    stdout.writeln('Could not parse port value "$val" into a number.');
+    exit(1);
+  });
+
   var handler = const shelf.Pipeline()
       .addMiddleware(shelf.logRequests())
       .addHandler(_echoRequest);
 
-  io.serve(handler, 'localhost', 8080).then((server) {
+  io.serve(handler, 'localhost', port).then((server) {
     print('Serving at http://${server.address.host}:${server.port}');
   });
 }
