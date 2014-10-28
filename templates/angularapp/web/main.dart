9b1beb02b72075c218a5d86d6bbda757a439d885
laco0416 <laco0416@gmail.com>
2014-10-28 09:39:10 +0900

    Fix app code #128

--- templates/angularapp/web/main.dart
+++ templates/angularapp/web/main.dart
@@ -1,8 +1,20 @@
+// Copyright (c) {{year}}, {{author}}. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
 import 'package:angular/angular.dart';
 import 'package:angular/application_factory.dart';
 
-import 'packages/{{projectName}}/main_app.dart';
+import 'package:{{projectName}}/main_app.dart';
+
+class MyAppModule extends Module {
+
+  MyAppModule() {
+    bind(MainApp);
+  }
+}
 
 void main() {
-  applicationFactory().rootContextType(MainApp).run();
+  applicationFactory()
+  .addModule(new MyAppModule())
+  .run();
 }
