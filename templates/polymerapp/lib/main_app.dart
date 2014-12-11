0a9bd004f6f6a8d101d4f632358630c8b2539b5c
Devon Carew <devoncarew@gmail.com>
2014-12-11 10:02:45 -0800

    upgrade some deps in the polymer sample

--- templates/polymerapp/lib/main_app.dart
+++ templates/polymerapp/lib/main_app.dart
@@ -1,8 +1,6 @@
 // Copyright (c) {{year}}, {{author}}. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
-import 'dart:html';
-
 import 'package:polymer/polymer.dart';
 
 /// A Polymer `<main-app>` element.
