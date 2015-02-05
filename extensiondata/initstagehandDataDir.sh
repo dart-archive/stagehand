b438c784e8ddec947859253ca5d080aef1c4d9fb
Kunal Deo <kunaldeo@gmail.com>
2015-02-05 14:52:43 +0530

    Fixed extensiondata folder name in stagehanddata directory installation script.

--- extensiondata/initstagehandDataDir.sh
+++ extensiondata/initstagehandDataDir.sh
@@ -2,7 +2,7 @@
 # this script should be run before using stagehand
 
 mkdir ~/stagehandData
-cd data
+cd extensiondata
 cp isloader.dart.data ~/stagehandData/isloader.dart
 cp pubspec.yaml.data ~/stagehandData/pubspec.yaml
 cd ~/stagehandData
