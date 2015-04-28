54017ed4979b1ea72d2a7cbea7984e298b97b127
Devon Carew <devoncarew@gmail.com>
2015-04-28 06:31:26 -0700

    fix copyright

--- templates/web-full/lib/reverser.dart
+++ templates/web-full/lib/reverser.dart
@@ -1,4 +1,4 @@
-// Copyright (c) 2015, <your name>. All rights reserved. Use of this source code
+// Copyright (c) {{year}}, {{author}}. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
 library reverser;
