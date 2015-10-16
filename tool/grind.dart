d3ec8a3f2eaf813d93bdd90a1125788ff259ed0e
Kevin Moore <kevmoo@google.com>
2015-10-15 21:12:44 -0700

    generate: yield const data
    Should not be writable

--- tool/grind.dart
+++ tool/grind.dart
@@ -71,7 +71,7 @@ void _concatenateFiles(Directory src, File target) {
 // All rights reserved. Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-List<String> data = [
+const List<String> data = const <String>[
 ${str}
 ];
 """);
