05386a997d61f0021519fe8f51f9d0a4e99461c5
Kleak <segaud.kevin@gmail.com>
2016-02-15 17:01:39 +0100

    remove useless comment and library keyword

--- templates/console-full/lib/__projectName__.dart
+++ templates/console-full/lib/__projectName__.dart
@@ -1,9 +1,6 @@
 // Copyright (c) __year__, __author__. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
-/// The __projectName__ library.
-library __projectName__;
-
 int calculate() {
   return 6 * 7;
 }
