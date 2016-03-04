1184618ceeed8fbdbf42c01a941d0fcb994c6b25
Kleak <segaud.kevin@gmail.com>
2016-03-04 10:06:20 +0100

    change comment to follow good library doc comment practices

--- templates/package-simple/lib/__projectName__.dart
+++ templates/package-simple/lib/__projectName__.dart
@@ -1,7 +1,9 @@
 // Copyright (c) __year__, __author__. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
-/// This is an awesome library. More dartdocs go here.
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
 library __projectName__;
 
 export 'src/__projectName___base.dart';
