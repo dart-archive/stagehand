93b737e413d673c5eb3e1bbd8b80a7bf5ad0c604
Filip Hracek <filip.hracek@gmail.com>
2016-08-19 12:06:20 -0700

    Add a component CSS file to the Angular template
    As proposed here:
    https://github.com/google/stagehand/issues/332

--- templates/web-angular/lib/app_component.dart
+++ templates/web-angular/lib/app_component.dart
@@ -3,5 +3,8 @@
 
 import 'package:angular2/core.dart';
 
-@Component(selector: 'my-app', templateUrl: 'app_component.html')
+@Component(
+    selector: 'my-app',
+    styleUrls: const ['app_component.css'],
+    templateUrl: 'app_component.html')
 class AppComponent {}
