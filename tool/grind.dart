e2551f75e53cee81fb1a7be0b26f85ff6ba70211
Devon Carew <devoncarew@google.com>
2016-12-01 10:17:41 -0800

    have the generated code use the current calendar year

--- tool/grind.dart
+++ tool/grind.dart
@@ -69,7 +69,7 @@ void _concatenateFiles(Directory src, File target) {
   String str = _traverse(src, '').map((s) => '  ${_toStr(s)}').join(',\n');
 
   target.writeAsStringSync("""
-// Copyright (c) 2014, Google Inc. Please see the AUTHORS file for details.
+// Copyright (c) ${_currentYear()}, Google Inc. Please see the AUTHORS file for details.
 // All rights reserved. Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
@@ -157,3 +157,5 @@ String _replaceInString(
       '\n' +
       source.substring(endIndex);
 }
+
+int _currentYear() => new DateTime.now().year;
