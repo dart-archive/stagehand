8759469416fdbd5976b421f3a694a162d3b9e003
Filip Hracek <filip.hracek@gmail.com>
2016-12-15 10:58:45 -0800

    Add material components to AngularDart template

--- templates/web-angular/lib/app_component.dart
+++ templates/web-angular/lib/app_component.dart
@@ -2,9 +2,18 @@
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
 import 'package:angular2/core.dart';
+import 'package:angular2_components/angular2_components.dart';
 
 @Component(
     selector: 'my-app',
     styleUrls: const ['app_component.css'],
-    templateUrl: 'app_component.html')
-class AppComponent {}
+    templateUrl: 'app_component.html',
+    directives: const [materialDirectives],
+    providers: const [materialProviders])
+class AppComponent {
+  /// Visibility state of the 'hello world' dialog.
+  bool helloVisible = false;
+
+  /// Name of user.
+  String name = "";
+}
