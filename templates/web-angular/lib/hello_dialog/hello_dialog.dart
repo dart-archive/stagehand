19c485c6e959126669af1f57b2f034a1cb643523
Filip Hracek <filip.hracek@gmail.com>
2016-12-16 18:24:50 -0800

    Apply dartfmt and sort

--- templates/web-angular/lib/hello_dialog/hello_dialog.dart
+++ templates/web-angular/lib/hello_dialog/hello_dialog.dart
@@ -5,23 +5,23 @@ import 'package:angular2/core.dart';
 import 'package:angular2_components/angular2_components.dart';
 
 @Component(
-    selector: 'hello-dialog',
-    styleUrls: const ['hello_dialog.css'],
-    templateUrl: 'hello_dialog.html',
-    directives: const [materialDirectives],
-    providers: const [materialProviders],
+  selector: 'hello-dialog',
+  styleUrls: const ['hello_dialog.css'],
+  templateUrl: 'hello_dialog.html',
+  directives: const [materialDirectives],
+  providers: const [materialProviders],
 )
 class HelloDialog {
   /// Modal component that hoists the inner MaterialDialog in a centered overlay.
   @ViewChild('wrappingModal')
   ModalComponent wrappingModal;
 
+  /// Name of user.
+  @Input()
+  String name = "";
+
   /// Opens the dialog.
   void open() {
     wrappingModal.open();
   }
-
-  /// Name of user.
-  @Input()
-  String name = "";
 }
