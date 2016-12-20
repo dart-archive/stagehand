898d91686262d5162cfdef17254bb24909b12d98
Filip Hracek <filip.hracek@gmail.com>
2016-12-20 14:24:32 -0800

    Apply dartfmt to annotations, remove unused attribute

--- templates/web-angular/lib/app_component.dart
+++ templates/web-angular/lib/app_component.dart
@@ -7,11 +7,11 @@ import 'package:angular2_components/angular2_components.dart';
 import 'package:__projectName__/hello_dialog/hello_dialog.dart';
 
 @Component(
-    selector: 'my-app',
-    styleUrls: const ['app_component.css'],
-    templateUrl: 'app_component.html',
-    directives: const [materialDirectives, HelloDialog],
-    providers: const [materialProviders],
+  selector: 'my-app',
+  styleUrls: const ['app_component.css'],
+  templateUrl: 'app_component.html',
+  directives: const [materialDirectives, HelloDialog],
+  providers: const [materialProviders],
 )
 class AppComponent {
   // Nothing here yet. All logic is in HelloDialog.
