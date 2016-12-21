7611219549e082eb13d05ff4a16ddf2316119f23
Filip Hracek <filiph@users.noreply.github.com>
2016-12-21 14:42:50 -0800

    Add components to the AngularDart template, and generally improve the template (#357)
    * Add material components to AngularDart template
    * Clean up the template
    * Add the two most common meta tags:
      * charset for server compatibility (<1% issue)
      * viewport for better scaling on mobile
    * Tighten dependencies
    * Apply dartfmt and sort
    * Apply dartfmt to annotations, remove unused attribute
    * Show more best practices:
      * Put anything non-trivial into its own component.
      * Components in separate directories.
      * Use #references and @ViewChild to simplify code.
      * Call components’ functions.
      * Put CSS as close to its use as possible.
      * Use trailing commas to simplify moving code around.

--- templates/web-angular/lib/app_component.dart
+++ templates/web-angular/lib/app_component.dart
@@ -2,9 +2,17 @@
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
 import 'package:angular2/core.dart';
+import 'package:angular2_components/angular2_components.dart';
+
+import 'package:__projectName__/hello_dialog/hello_dialog.dart';
 
 @Component(
-    selector: 'my-app',
-    styleUrls: const ['app_component.css'],
-    templateUrl: 'app_component.html')
-class AppComponent {}
+  selector: 'my-app',
+  styleUrls: const ['app_component.css'],
+  templateUrl: 'app_component.html',
+  directives: const [materialDirectives, HelloDialog],
+  providers: const [materialProviders],
+)
+class AppComponent {
+  // Nothing here yet. All logic is in HelloDialog.
+}
---
+++ templates/web-angular/lib/hello_dialog/hello_dialog.dart
@@ -0,0 +1,27 @@
+// Copyright (c) __year__, __author__. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+import 'package:angular2/core.dart';
+import 'package:angular2_components/angular2_components.dart';
+
+@Component(
+  selector: 'hello-dialog',
+  styleUrls: const ['hello_dialog.css'],
+  templateUrl: 'hello_dialog.html',
+  directives: const [materialDirectives],
+  providers: const [materialProviders],
+)
+class HelloDialog {
+  /// Modal component that hosts the inner MaterialDialog in a centered overlay.
+  @ViewChild('wrappingModal')
+  ModalComponent wrappingModal;
+
+  /// Name of user.
+  @Input()
+  String name = "";
+
+  /// Opens the dialog.
+  void open() {
+    wrappingModal.open();
+  }
+}
