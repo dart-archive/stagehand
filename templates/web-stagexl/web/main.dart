d268114819cbffb18ae6ac1db440c233efdceb2e
Tomucha <tomucha@gmail.com>
2017-03-20 21:35:15 +0100

    Added some interaction as @kevmoo requested.

--- templates/web-stagexl/web/main.dart
+++ templates/web-stagexl/web/main.dart
@@ -2,16 +2,16 @@
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
 import 'dart:async';
-import 'dart:html';
+import 'dart:html' as html;
 import 'package:stagexl/stagexl.dart';
+import 'dart:math';
 
 Future<Null> main() async {
-
   StageOptions options = new StageOptions()
     ..backgroundColor = Color.White
     ..renderEngine = RenderEngine.WebGL;
 
-  var canvas = querySelector('#stage');
+  var canvas = html.querySelector('#stage');
   var stage = new Stage(canvas, width: 1280, height: 800, options: options);
 
   var renderLoop = new RenderLoop();
@@ -23,7 +23,8 @@ Future<Null> main() async {
   await resourceManager.load();
 
   var logoData = resourceManager.getBitmapData("dart");
-  var logo = new Bitmap(logoData);
+  var logo = new Sprite();
+  logo.addChild(new Bitmap(logoData));
 
   logo.pivotX = logoData.width / 2;
   logo.pivotY = logoData.height / 2;
@@ -38,7 +39,17 @@ Future<Null> main() async {
   var tween = stage.juggler.addTween(logo, 3, Transition.easeOutBounce);
   tween.animate.y.to(800 / 2);
 
+  // Add some interaction on mouse click.
+  Tween rotation;
+  logo.onMouseClick.listen((MouseEvent e) {
+    // Don't run more rotations at the same time.
+    if (rotation != null) return;
+    rotation = stage.juggler.addTween(logo, 0.5, Transition.easeInOutCubic);
+    rotation.animate.rotation.by(2 * PI);
+    rotation.onComplete = () => rotation = null;
+  });
+  logo.mouseCursor = MouseCursor.POINTER;
+
   // See more examples:
   // https://github.com/bp74/StageXL_Samples
-
 }
