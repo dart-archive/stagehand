ca9a4338f5fb8151e618effe039ca27e8e1cd803
Patrice Chalin <pchalin@gmail.com>
2017-05-05 12:55:51 -0400

    web-angular template: add tests, enhance demo
    Fixes #396

--- templates/web-angular/lib/cheer_dialog/cheer_dialog.dart
+++ templates/web-angular/lib/cheer_dialog/cheer_dialog.dart
@@ -1,28 +1,29 @@
 // Copyright (c) __year__, __author__. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
+import 'dart:async';
+
 import 'package:angular2/core.dart';
 import 'package:angular_components/angular_components.dart';
 
 @Component(
-  selector: 'hello-dialog',
-  styleUrls: const ['hello_dialog.css'],
-  templateUrl: 'hello_dialog.html',
+  selector: 'cheer-dialog',
+  styleUrls: const ['cheer_dialog.css'],
+  templateUrl: 'cheer_dialog.html',
   directives: const [materialDirectives],
-  providers: const [materialProviders],
 )
-class HelloDialog {
+class CheerDialog {
   /// Modal component that hosts the inner MaterialDialog in a centered overlay.
   @ViewChild('wrappingModal')
   ModalComponent wrappingModal;
 
   /// Name of user.
-  @Input()
-  String name = "";
+  String name = '';
 
-  /// Opens the dialog.
-  void open([String name]) {
-    if (name != null) this.name = name;
+  void open(String name) {
+    this.name = name;
     wrappingModal.open();
   }
+
+  Future<bool> close() => wrappingModal.close();
 }
