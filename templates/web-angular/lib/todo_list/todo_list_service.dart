3748fa04bf8027a2163fbffa8f00c5b0500a7a6f
Patrice Chalin <chalin@users.noreply.github.com>
2017-05-09 19:20:04 -0400

    web-angular: start with an empty todo list (#407)

--- templates/web-angular/lib/todo_list/todo_list_service.dart
+++ templates/web-angular/lib/todo_list/todo_list_service.dart
@@ -5,7 +5,7 @@ import 'package:angular2/core.dart';
 /// Mock service emulating access to a to-do list stored on a server.
 @Injectable()
 class TodoListService {
-  List<String> mockTodoList = <String>['Feed the cats', 'Walk the dogs'];
+  List<String> mockTodoList = <String>[];
 
   Future<List<String>> getTodoList() async => mockTodoList;
 }
