4a51962a56ad7db4eadb1a564a94a8b5d0ad3414
Matan Lurey <matan@lurey.org>
2017-05-15 17:56:30 -0700

    Remove platform* options.

--- templates/web-angular/lib/app_component.dart
+++ templates/web-angular/lib/app_component.dart
@@ -1,7 +1,7 @@
 // Copyright (c) __year__, __author__. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
-import 'package:angular2/core.dart';
+import 'package:angular2/angular2.dart';
 import 'package:angular_components/angular_components.dart';
 
 import 'todo_list/todo_list_component.dart';
