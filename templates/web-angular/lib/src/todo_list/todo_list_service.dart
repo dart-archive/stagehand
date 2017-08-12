be82e11007a23163255cb106be71f311f41e801f
Kathy Walrath <kathyw@google.com>
2017-08-11 17:48:05 -0700

    Add copyright to todo_list_service.dart
    Fixes #433

--- templates/web-angular/lib/src/todo_list/todo_list_service.dart
+++ templates/web-angular/lib/src/todo_list/todo_list_service.dart
@@ -1,3 +1,6 @@
+// Copyright (c) __year__, __author__. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
 import 'dart:async';
 
 import 'package:angular2/core.dart';
