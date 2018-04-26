7f6e14a406c213e4810fd5a561ced8a27f9fc4a4
Patrice Chalin <pchalin@gmail.com>
2018-04-26 10:53:19 -0400

    web-angular tests: drop use of page object class and pageloader mock
    Fixes #487

--- templates/web-angular/test/app_test.dart
+++ templates/web-angular/test/app_test.dart
@@ -1,42 +1,21 @@
 @TestOn('browser')
-import 'dart:async';
-
 import 'package:angular_test/angular_test.dart';
-import 'package:pageloader/objects.dart';
 import 'package:test/test.dart';
 import 'package:__projectName__/app_component.dart';
-
-import 'app_test.template.dart' as ng;
-
-NgTestFixture<AppComponent> fixture;
-AppPO appPO;
+import 'package:__projectName__/app_component.template.dart' as ng;
 
 void main() {
-  ng.initReflector();
-  final testBed = new NgTestBed<AppComponent>();
+  final testBed =
+      NgTestBed.forComponent<AppComponent>(ng.AppComponentNgFactory);
+  NgTestFixture<AppComponent> fixture;
 
   setUp(() async {
     fixture = await testBed.create();
-    // TODO: temporary. https://github.com/dart-lang/site-webdev/issues/1351
-    appPO = await new AppPO().resolve(fixture);
-    // appPO = await fixture.resolvePageObject(AppPO);
   });
 
   tearDown(disposeAnyRunningTest);
 
-  test('title', () async {
-    expect(await appPO.title, 'My First AngularDart App');
+  test('heading', () {
+    expect(fixture.text, contains('My First AngularDart App'));
   });
-
-  // Testing info: https://webdev.dartlang.org/angular/guide/testing
-}
-
-class AppPO
-    // TODO: temporary. https://github.com/dart-lang/site-webdev/issues/1351
-    extends PageObjectBase //
-{
-  @ByTagName('h1')
-  PageLoaderElement get _title => q('h1');
-
-  Future<String> get title => _title.visibleText;
 }
