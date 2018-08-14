0244f6b6d36b28b4103b698403328cc869d226e9
Kevin Moore <kevmoo@google.com>
2018-08-14 12:23:36 -0700

    misc: use the latest pkg:source_gen

--- tool/builder.dart
+++ tool/builder.dart
@@ -11,12 +11,4 @@ import 'src/version_generator.dart';
 Builder stagehandBuilder([_]) => new PartBuilder([
       new VersionGenerator(),
       new DataGenerator(),
-    ], header: _fileHeader);
-
-final _fileHeader = '''
-// Copyright (c) 2018, Google Inc. Please see the AUTHORS file for details.
-// All rights reserved. Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
-// GENERATED CODE - DO NOT MODIFY BY HAND
-''';
+    ], '.g.dart');
