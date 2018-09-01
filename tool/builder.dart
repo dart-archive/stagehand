31250f97be8ba92c1ef5c4a8648d543b55cf9665
Kevin Moore <kevmoo@users.noreply.github.com>
2018-09-01 12:26:10 -0700

    Migrate version logic to pkg:build_version (#579)

--- tool/builder.dart
+++ tool/builder.dart
@@ -6,9 +6,7 @@ import 'package:source_gen/source_gen.dart';
 import 'package:build/build.dart';
 
 import 'src/code_generator.dart';
-import 'src/version_generator.dart';
 
 Builder stagehandBuilder([_]) => new PartBuilder([
-      new VersionGenerator(),
       new DataGenerator(),
     ], '.g.dart');
