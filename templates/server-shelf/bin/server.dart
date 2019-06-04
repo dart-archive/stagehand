1271b295fd5dd884d455731deabe0bb8ba81ca7d
Brett Morgan <brettmorgan@google.com>
2019-06-04 12:10:58 +1000

    Make server-shelf compatible with Cloud Run

--- templates/server-shelf/bin/server.dart
+++ templates/server-shelf/bin/server.dart
@@ -5,15 +5,15 @@ import 'package:shelf/shelf.dart' as shelf;
 import 'package:shelf/shelf_io.dart' as io;
 
 main(List<String> args) async {
-  var parser = ArgParser()..addOption('port', abbr: 'p', defaultsTo: '8080');
-
+  var parser = ArgParser()..addOption('port', abbr: 'p');
   var result = parser.parse(args);
 
-  var port = int.tryParse(result['port']);
+  var portStr = result['port'] ?? Platform.environment['PORT'] ?? '8080';
+  var port = int.tryParse(portStr);
 
   if (port == null) {
     stdout.writeln(
-        'Could not parse port value "${result['port']}" into a number.');
+        'Could not parse port value "$portStr" into a number.');
     // 64: command line usage error
     exitCode = 64;
     return;
@@ -23,7 +23,7 @@ main(List<String> args) async {
       .addMiddleware(shelf.logRequests())
       .addHandler(_echoRequest);
 
-  var server = await io.serve(handler, 'localhost', port);
+  var server = await io.serve(handler, '0.0.0.0', port);
   print('Serving at http://${server.address.host}:${server.port}');
 }
 
