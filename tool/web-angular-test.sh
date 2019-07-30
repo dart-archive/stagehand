23265716609be13b73b41596263286dafdef8c05
John Ryan <ryjohn@google.com>
2019-07-30 16:33:41 -0700

    remove xvfb from web-angular-test.sh (#624)
    Fixes #623

--- tool/web-angular-test.sh
+++ tool/web-angular-test.sh
@@ -12,9 +12,6 @@ source ./tool/env-set.sh
 # Run component tests for web-angular.
 pushd templates/web-angular
 
-sh -e /etc/init.d/xvfb start
-t=0; until (xdpyinfo -display :99 &> /dev/null || test $t -gt 10); do sleep 1; let t=$t+1; done
-
 travis_fold start web_angular.pub
   (set -x; pub get)
 travis_fold end web_angular.pub
