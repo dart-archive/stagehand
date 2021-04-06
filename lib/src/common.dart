04d408f897ff73a0ff773e76c2b09b9721c988ec
Michael Thomsen <mit@google.com>
2021-04-06 20:33:17 +0200

    Opt out of null safety

--- lib/src/common.dart
+++ lib/src/common.dart
@@ -2,6 +2,8 @@
 // All rights reserved. Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// @dart=2.9
+
 /// Some utility methods for stagehand.
 import 'dart:convert' show base64, utf8;
 
