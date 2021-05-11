d24023909720edd22c4ecb6634b4ee9092b1d850
Brett Morgan <brett.morgan@gmail.com>
2021-05-11 02:43:35 +0000

    Whups, git merge fail

--- tool/builder.dart
+++ tool/builder.dart
@@ -3,10 +3,6 @@
 // BSD-style license that can be found in the LICENSE file.
 
 // @dart=2.9
-<<<<<<< HEAD
-
-=======
->>>>>>> ff7988e6909007c22f00c25053b4e14461444421
 import 'package:build/build.dart';
 import 'package:source_gen/source_gen.dart';
 
